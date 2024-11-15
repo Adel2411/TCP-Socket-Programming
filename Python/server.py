5d9aaf249ff5afbcfe54266976ad3837fc710f51
Adel2411 <hadjarabadel.2411@gmail.com>
2024-11-16 00:09:23 +0100

    Finished server implementation in Python

---
+++ Python/server.py
@@ -0,0 +1,40 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+
+def main():
+    # Create a socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the address and port
+    server.bind((HOST, PORT))
+
+    # Set the accepted connections at one time
+    server.listen(3)
+
+    print(f"Server is listening on {HOST}:{PORT}...")
+
+    # Accept the connection
+    client, address = server.accept()
+    print(f"Connected from {address}")
+
+    while True:
+        # Receive the data from the client
+        data = client.recv(1024).decode()
+        if not data or data.lower() == "end":
+            print("Connection is closed by the client")
+            break
+        print(f"Client: {data}")
+
+        # Send a reply to the client
+        reply = input("Server (type your message): ")
+        client.send(reply.encode("utf-8"))
+
+    # Close the connection
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
