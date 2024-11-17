96e8123f650f373e2fee3c6f6459e5ed74517740
Adel2411 <hadjarabadel.2411@gmail.com>
2024-11-17 01:43:54 +0100

    Finished server implementation in Rust

---
+++ Rust/server.rs
@@ -0,0 +1,43 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()> {
+    //  Bind the server to the address
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("Server listening on port 8080");
+
+    // Accept single incoming connection
+    let (mut stream, addr) = listener.accept()?;
+    println!("Connection received from {}", addr);
+
+    // Send welcome message to the client
+    let mut buffer = [0; 1024];
+    let welcome_message = "Welcome to the server!";
+    stream.write_all(welcome_message.as_bytes())?;
+
+    // Communication loop
+    loop {
+        // Read the incoming data
+        let bytes_read = stream.read(&mut buffer)?;
+        if bytes_read == 0 {
+            println!("Connection closed");
+            break;
+        }
+
+        let message = String::from_utf8_lossy(&buffer[..bytes_read]);
+        println!("Client : {}", message);
+
+        if message.trim().to_lowercase() == "end" {
+            println!("Closing the connection");
+            break;
+        }
+
+        // Echo the data back to the client
+        stream.write_all(message.as_bytes())?;
+    }
+
+    // Close the connection
+    stream.shutdown(std::net::Shutdown::Both)?;
+
+    Ok(())
+}
